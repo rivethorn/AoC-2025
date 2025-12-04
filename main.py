1f578aae1f6335fc8c82538fdda159dd65f324bb
Hassan Qasemi <hassanak813@outlook.com>
2025-12-04 21:06:25 +0330

    Added logging

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import sys
+import logging
+import time
+
+logging.basicConfig(
+    format="%(asctime)s.%(msecs)03d:%(levelname)s:%(name)s:\t%(message)s",
+    datefmt="%H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def main() -> None:
     print("=== Advent of Code 2025 ===")
+    print("———————————————————————————")
 
     if len(sys.argv) > 1:
         match sys.argv[1]:
@@ -28,4 +38,8 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    t1 = time.perf_counter()
     main()
+    t2 = time.perf_counter()
+    print("———————————————————————————")
+    logger.info("Execution time: %0.4f seconds", t2 - t1)
